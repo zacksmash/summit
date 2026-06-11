5e15e2ed4291517e91862d5a3b382cf0a7dfb0bb
Zack Warren <zachary.warren@me.com>
2026-06-11 16:00:52 -0600

    Initial Commit

---
+++ resources/js/components/TeamInvitationAlert.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Info } from '@lucide/vue';
+import { Alert, AlertDescription } from '@/components/ui/alert';
+import type { TeamInvitationContext } from '@/types';
+
+type Props = {
+    invitation: TeamInvitationContext;
+    action: 'Log in' | 'Register';
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+    <div data-test="team-invitation-alert">
+        <Alert
+            class="border-blue-200 bg-blue-50 text-blue-900 dark:border-blue-900/50 dark:bg-blue-950/50 dark:text-blue-100 [&>svg]:text-blue-600 dark:[&>svg]:text-blue-400"
+        >
+            <Info class="size-4" />
+            <AlertDescription class="text-blue-900 dark:text-blue-100">
+                {{ action }} to join the "{{ invitation.teamName }}" team.
+            </AlertDescription>
+        </Alert>
+    </div>
+</template>
